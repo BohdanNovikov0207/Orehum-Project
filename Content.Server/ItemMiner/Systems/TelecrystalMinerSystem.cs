e6e2fe20adfd4f462ae498d18b2925fc695559f0
Serj <fil.sergey2008@gmail.com>
2025-08-20 23:59:39 +0700

    stealing TelecrystalMinerSystem.cs
    Signed-off-by: Serj <fil.sergey2008@gmail.com>

--- Content.Server/ItemMiner/Systems/TelecrystalMinerSystem.cs
+++ Content.Server/ItemMiner/Systems/TelecrystalMinerSystem.cs
@@ -1,3 +1,8 @@
+// SPDX-FileCopyrightText: 2025 GoobBot <uristmchands@proton.me>
+// SPDX-FileCopyrightText: 2025 Ilya246 <ilyukarno@gmail.com>
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
 using Content.Shared.ItemMiner;
 using Content.Server.Chat.Systems;
 using Content.Server.Station.Components;
