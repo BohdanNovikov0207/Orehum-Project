12ee8d5c4caa2cb2a98ef1991b1bb64f8274949c
BadRyuner <54708336+BadRyuner@users.noreply.github.com>
2025-09-06 15:37:32 +0300

    new poison food psionic power

--- Content.Shared/_Orehum/Psionic/Events.cs
+++ Content.Shared/_Orehum/Psionic/Events.cs
@@ -1,8 +1,7 @@
 ﻿using Content.Shared.Actions;
 
-
 namespace Content.Shared._Orehum.Psionic;
 
-public sealed partial class EatPsionicActionEvent : EntityTargetActionEvent
-{
-}
+public sealed partial class EatPsionicActionEvent : EntityTargetActionEvent;
+
+public sealed partial class PoisonFoodActionEvent : EntityTargetActionEvent;
