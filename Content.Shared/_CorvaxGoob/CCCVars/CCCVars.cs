072546e95604958a960c8cdb24f087c205fd676c
nukkuminen <90336027+oldschoolotaku@users.noreply.github.com>
2025-11-11 19:21:32 +0300

    [PORT] Передача предметов в руки (#264)
    * take files from original PR
    
    * it works somehow?
    
    * rebase everything to corvaxgoob
    
    * resolving conflict
    
    * IPC and skeleton fix

--- Content.Shared/_CorvaxGoob/CCCVars/CCCVars.cs
+++ Content.Shared/_CorvaxGoob/CCCVars/CCCVars.cs
@@ -23,4 +23,12 @@ public sealed class CCCVars
     public static readonly CVarDef<bool> StationGoal =
         CVarDef.Create("game.station_goal", true, CVar.SERVERONLY);
 
+    /// <summary>
+    /// Offer item.
+    /// </summary>
+    public static readonly CVarDef<bool> OfferModeIndicatorsPointShow =
+        CVarDef.Create("hud.offer_mode_indicators_point_show", true, CVar.ARCHIVE | CVar.CLIENTONLY);
+
+    public static readonly CVarDef<bool> CombatModeSoundEnabled =
+        CVarDef.Create("audio.combat_mode_sound_enabled", true, CVar.ARCHIVE | CVar.CLIENTONLY);
 }
