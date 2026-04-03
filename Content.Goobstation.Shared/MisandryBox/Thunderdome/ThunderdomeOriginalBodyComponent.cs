Simple-Station/Einstein-Engines

Thunderdome (#6066)
* cuh

* esword kit + give everyone a basic medkit by default

* cache entman

* unmolest rulesystem

* Everywhere I look

* todo

* todo2

* fuck you

* ass

* Shitcode1

* stuff 2

* stuff 3

* stuff 4

* ass2

* my IDE got fucked you get webedits

whatever

---------

Co-authored-by: notactuallymarty <martynashagriefer@gmail.com>
Co-authored-by: Marty <27968892+NotActuallyMarty@users.noreply.github.com>

Content.Goobstation.Shared/MisandryBox/Thunderdome/ThunderdomeOriginalBodyComponent.cs
@@ -1,0 +1,12 @@
+using Robust.Shared.GameObjects;
+using Robust.Shared.Network;
+using Robust.Shared.Serialization.Manager.Attributes;
+
+namespace Content.Goobstation.Shared.MisandryBox.Thunderdome;
+
+[RegisterComponent]
+public sealed partial class ThunderdomeOriginalBodyComponent : Component
+{
+    [DataField]
+    public NetUserId Owner;
+}

BohdanNovikov0207/Orehum-Project

Content.Goobstation.Shared/MisandryBox/Thunderdome/ThunderdomeOriginalBodyComponent.cs
@@ -1,0 +1,12 @@
+using Robust.Shared.GameObjects;
+using Robust.Shared.Network;
+using Robust.Shared.Serialization.Manager.Attributes;
+
+namespace Content.Goobstation.Shared.MisandryBox.Thunderdome;
+
+[RegisterComponent]
+public sealed partial class ThunderdomeOriginalBodyComponent : Component
+{
+    [DataField]
+    public NetUserId UserId;
+}