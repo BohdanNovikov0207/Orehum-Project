3929b98081fed376b55d8b7c175628c7767c9c51
IsNotAcceptable <184727191+IsNotAcceptable@users.noreply.github.com>
2026-04-05 14:32:24 +0700

    LOX

--- Scripts/sh/runTestsYAML.sh
+++ Scripts/sh/runTestsYAML.sh
@@ -5,10 +5,4 @@
 
 cd ../../
 
-mkdir Scripts/logs
-
-rm Scripts/logs/Content.YAMLLinter.log
-dotnet run --project Content.YAMLLinter/Content.YAMLLinter.csproj -c DebugOpt -- NUnit.ConsoleOut=0 > Scripts/logs/Content.YAMLLinter.log
-
-echo "Tests complete. Press enter to continue."
-read
+dotnet run --project Content.YAMLLinter/Content.YAMLLinter.csproj --no-build
