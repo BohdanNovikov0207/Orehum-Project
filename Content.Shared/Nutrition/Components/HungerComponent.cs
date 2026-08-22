096087c053d2439a8a25ec26fc8820f2d1030300
Bogdan Novikov <150541068+BohdanNovikov0207@users.noreply.github.com>
2026-08-22 15:49:34 +0300

    Change BaseDecayRate from 0.033 to 0.017
    Updated the base decay rate for hunger component.
    
    Signed-off-by: Bogdan Novikov <150541068+BohdanNovikov0207@users.noreply.github.com>

--- Content.Shared/Nutrition/Components/HungerComponent.cs
+++ Content.Shared/Nutrition/Components/HungerComponent.cs
@@ -36,7 +36,7 @@ public sealed partial class HungerComponent : Component
     /// </summary>
     /// <remarks>Any time this is modified, <see cref="HungerSystem.SetAuthoritativeHungerValue"/> should be called.</remarks>
     [DataField("baseDecayRate"), ViewVariables(VVAccess.ReadWrite)]
-    public float BaseDecayRate = 0.0333222222f; // Orehum
+    public float BaseDecayRate = 0.01666666666f; // Orehum
 
     /// <summary>
     /// The actual amount at which <see cref="LastAuthoritativeHungerValue"/> decays.
